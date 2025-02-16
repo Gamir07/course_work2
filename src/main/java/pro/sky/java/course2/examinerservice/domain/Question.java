8fee8046ebe10f57f03c74f5d85d5f78e897f9ce
Gamir <gamir07@yandex.ru>
2025-02-16 11:42:56 +0300

    change class Question, add space symbol in toString method

--- src/main/java/pro/sky/java/course2/examinerservice/domain/Question.java
+++ src/main/java/pro/sky/java/course2/examinerservice/domain/Question.java
@@ -5,8 +5,8 @@ public record Question(String question, String answer) {
     @Override
     public String toString() {
         return "Question{" +
-               "question='" + question + '\'' +
-               ", answer='" + answer + '\'' +
+               "question ='" + question + '\'' +
+               ", answer ='" + answer + '\'' +
                '}';
     }
 }
